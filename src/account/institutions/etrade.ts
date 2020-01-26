91ea26220fe63d4ce875d7f6e43f4d0323a01a03
Jesse Aldridge <jessealdridge@gmail.com>
2020-01-26 03:29:55 -0800

    write transactions to timestamped file

--- src/account/institutions/etrade.ts
+++ src/account/institutions/etrade.ts
@@ -1,8 +1,13 @@
+import fs from 'fs';
 import path from 'path';
+
 import shell from 'shelljs';
+import { Page } from 'puppeteer';
+import Papa from 'papaparse';
+import moment from 'moment';
+import glob from 'glob';
 
 import { BrowserUtil, log, sleep, getDownloadDir } from '../../util';
-import { Page } from 'puppeteer';
 import Account from '../account';
 import Ledger from '../ledger';
 
@@ -23,6 +28,53 @@ const html = `<html style="font-family: monospace;">
     </body>
 </html>`;
 
+function expandHomeDir (path_: string) {
+  var homedir: string = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'] || '';
+  if (!path_) return path_;
+  if (path_ == '~') return homedir;
+  if (path_.slice(0, 2) != '~/') return path_;
+  return path.join(homedir, path_.slice(2));
+}
+
+
+function write_timestamped_file(root_dir_path: string, filename: string, obj: any) {
+  root_dir_path = expandHomeDir(root_dir_path)
+  const date_str = moment(new Date()).local().format('YYYY-MM-DD_hh-mm')
+  const dir_path = path.join(root_dir_path, date_str)
+  return write(dir_path, filename, obj)
+}
+
+function write(dir_path:string, filename:string, obj:any) {
+  if(!fs.existsSync(dir_path))
+    // is sync
+    shell.mkdir('-p', dir_path);
+
+  const file_path = path.join(dir_path, filename)
+
+  const split = filename.split('.')
+  const exten = split[split.length - 1]
+  let text = null
+  if(exten == 'csv')
+    text = Papa.unparse(obj)
+  else
+    text = JSON.stringify(obj, null, 2)
+
+  fs.writeFileSync(file_path, text)
+  return file_path
+}
+
+function get_last_timestamped_dir_path(root_dir_path:string) {
+  root_dir_path = expandHomeDir(root_dir_path)
+  const date_paths = glob.sync(path.join(root_dir_path, '2*'))
+  date_paths.sort()
+  return date_paths[date_paths.length - 1]
+}
+
+function write_to_last_timestamped_dir(root_dir_path: string, filename:string, obj:any) {
+  write(get_last_timestamped_dir_path(root_dir_path), filename, obj)
+}
+
+
 export default class Etrade implements Account {
     private static DISPLAY_NAME = 'Etrade';
     public displayName = Etrade.DISPLAY_NAME;
@@ -32,7 +84,6 @@ export default class Etrade implements Account {
         await this.removeOldTransactionFiles();
         await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
         await this.login(page);
-        // await page.screenshot({path: 'example.png'});
         let balance = await this.getBalance(page);
         await this.downloadTransactions(page);
         log.succeed('transactions downloaded');
@@ -79,12 +130,32 @@ export default class Etrade implements Account {
     }
 
     async downloadTransactions(page: Page): Promise<void> {
-        await page.goto('https://us.etrade.com/e/t/invest/downloadofxtransactions?fp=TH');
-        await page.waitForSelector('input[value="msexcel"]', {visible: true})
-        await page.click('input[value="msexcel"]')
-        await page.waitForSelector('input[alt="Download"]', {visible: true})
-        await page.click('input[alt="Download"]')
-        await sleep(1000)
+        log.succeed('downloading transactions...')
+
+        await page.goto(
+            'https://us.etrade.com/e/t/accounts/txnhistory?TXN=AccountTrans&ploc=2017-nav-Accounts'
+        )
+
+        const transaction_rows = await page.evaluate(() => {
+            const transaction_rows = []
+            let trs = document.querySelectorAll('table.tBorder tr')
+            for(let irow = 0; irow < trs.length; irow++) {
+                const tds = trs[irow].querySelectorAll('td')
+                if(tds.length != 4)
+                    continue
+                const transaction_row: string[] = []
+                transaction_rows.push(transaction_row)
+                for(let icol = 0; icol < tds.length; icol++) {
+                    const text = tds[icol].textContent || ''
+                    transaction_row.push(text.trim())
+                }
+            }
+
+            return transaction_rows
+        });
+
+        write_timestamped_file('~/etrade-data', 'transactions.json', transaction_rows);
+        log.succeed('downloaded transactions')
     }
 
     async getVipToken(page: Page): Promise<string> {
